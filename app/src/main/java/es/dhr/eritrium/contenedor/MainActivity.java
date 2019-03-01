19ee0d489bde5d7118a08b0af9197d72ffd8259a
dherrero1974 <herrero.daniel@gmail.com>
2019-03-01 19:50:13 +0100

    Initial commit

---
+++ app/src/main/java/es/dhr/eritrium/contenedor/MainActivity.java
@@ -0,0 +1,40 @@
+package es.dhr.eritrium.contenedor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView)findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("http://192.168.1.2:8080/eritrium");
+        //myWebView.loadUrl("http://www.elmundo.es");
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        WebView mWebView;
+        mWebView = (WebView) findViewById(R.id.webview);
+        if (event.getAction() == KeyEvent.ACTION_DOWN) {
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_BACK:
+                    if (mWebView.canGoBack()) {
+                        mWebView.goBack();
+                    } else {
+                        finish();
+                    }
+                    return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
