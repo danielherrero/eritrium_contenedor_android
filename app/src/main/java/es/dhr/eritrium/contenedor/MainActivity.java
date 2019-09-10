141480945a87db1dfbc27c6f0134fcecc08157e3
dherrero1974 <herrero.daniel@gmail.com>
2019-09-10 19:43:25 +0200

    no message

--- app/src/main/java/es/dhr/eritrium/contenedor/MainActivity.java
+++ app/src/main/java/es/dhr/eritrium/contenedor/MainActivity.java
@@ -3,21 +3,25 @@ package es.dhr.eritrium.contenedor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
         WebView myWebView = (WebView)findViewById(R.id.webview);
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         myWebView.setWebViewClient(new WebViewClient());
-        myWebView.loadUrl("http://192.168.1.2:8080/eritrium");
+        myWebView.loadUrl("http://demo.strumbox.com.mx");
         //myWebView.loadUrl("http://www.elmundo.es");
     }
     @Override
